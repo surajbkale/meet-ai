c153c3fb82c364a949d6a3543c79522e8a40302d
SurajKale01 <surajkalecsit@gmail.com>
2025-09-17 15:30:30 +0530

    setup

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,5 @@
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
-  return (
-    <div className="text-3xl font-bold underline text-red-500">Hello World</div>
-  );
+  return <Button variant="destructive">Click me</Button>;
 }
